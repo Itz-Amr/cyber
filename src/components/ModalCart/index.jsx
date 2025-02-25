91faef54ac3e36549703283c38e5f615d62f5e7d
Itz-Amr <khufu701@gmail.com>
2025-02-25 19:30:08 +0200

    commit

--- src/components/ModalCart/index.jsx
+++ src/components/ModalCart/index.jsx
@@ -3,6 +3,7 @@ import styles from "./index.module.css";
 import test from "../../assets/images/product_1.png";
 import { FiPlus } from "react-icons/fi";
 import { FiMinus } from "react-icons/fi";
+import { IoMdClose } from "react-icons/io";
 
 export default function ModalCart() {
   const { closeModalCart, productsCart } = showModalCart();
@@ -10,23 +11,24 @@ export default function ModalCart() {
     <div id={styles.modalCart} onClick={closeModalCart}>
       <div
         onClick={(e) => e.stopPropagation()}
-        className="col-6 bg-white p-3 d-flex flex-column rounded animate__animated animate__fadeIn"
+        className="bg-white p-3 d-flex flex-column gap-3 rounded animate__animated animate__fadeIn"
       >
         <h1 className="col-12 d-flex justify-content-center">Shopping Cart</h1>
         {productsCart.map((el, index) => {
           return (
-            <div key={index} className="col-12 d-flex justify-content-between">
+            <div key={index} id={styles.cart}>
+              <IoMdClose className={styles.exitIcon} />
+              <img src={test} />
+
+              <h5>Product Name: {el.name}</h5>
+
               <div>
-                <img src={test} />
-                <h5>{el.name}</h5>
-              </div>
-
-              <span>Price :{el.price}$</span>
-
-              <div className="d-flex gap-2 align-items-center">
-                <FiPlus />
-                <span>{el.qty}</span>
-                <FiMinus />
+                <p>{el.price}$</p>
+                <div>
+                  <FiMinus className={styles.icon} />
+                  <span>{el.qty}</span>
+                  <FiPlus className={styles.icon} />
+                </div>
               </div>
             </div>
           );
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -10,7 +10,7 @@ export const showModalCart = create((set) => ({
   showCart: false,
   productsCart: [
     { name: "iPhone X", price: 400, qty: 3 },
-    // { name: "iPhone 10", price: 500, qty: 3 },
+    { name: "iPhone 10", price: 500, qty: 3 },
     // { name: "iPhone 11", price: 600, qty: 3 },
     // { name: "iPhone 12", price: 700, qty: 3 },
   ],
